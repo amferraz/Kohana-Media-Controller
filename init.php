852464b069043ad309337fac411276b5110b60c0
Claudio Albertin <albertin.claudio@gmail.com>
2011-05-21 14:53:31 +0200

    Fix route

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::set('media', 'media/<action>/<path>(.<format>)', array('path' => '.*', 'format' => '\w+'))
+Route::set('media', 'media/<action>/<path>(.<format>)', array('format' => '\w+'))
 	->defaults(array(
 		'controller' => 'media',
 		'action'     => 'index',
